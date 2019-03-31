5946dd1e3feda6effdf085319cebaac9c4906409
João Felipe Pimentel <joaofelipenp@gmail.com>
2019-03-31 20:52:09 -0300

    Update package on PyPi

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except (IOError, ImportError):
 
 setup(
     name="Snowballing",
-    version="0.2.1",
+    version="0.2.2",
     url="https://github.com/JoaoFelipe/snowballing",
     description="Provides tools for literature snowballing",
     long_description=long_description,
